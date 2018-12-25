a34260cc806dd325a7ea37dfc5bd23443f07b165
vivekjain123 <contactvivekjain@gmail.com>
2018-12-25 04:24:51 +0100

    added more examples into OOPs basics

--- src/main/scala/objectOriented/OOBasics.scala
+++ src/main/scala/objectOriented/OOBasics.scala
@@ -6,6 +6,17 @@ object OOBasics extends App{
   println(person.x)
   println(person.greet("vivek"))
   println(person.greet())
+ val aa = new Writer("Gregory David", "Roberts", 1952)
+  println(aa.yearOfBirth)
+  println(aa.fullName)
+  
+  val nn = new Novel("shantaram",2003,aa)
+  println(nn.authorAge)
+  println(nn.isWrittenBy(aa))
+  
+  val cc = new Counter
+  cc.inc.print
+  
   
 }
 
@@ -25,3 +36,53 @@ def greet(name: String): Unit = println(s"${this.name} says hi $name")
 //multiple constructors
   def this(name:String) = this(name,0)
 }
+
+/* Create novel and writer class. Write class should have three params
+ * first name , surname and year of birth.
+ *  A method called fullname which return concatenation of firstname and surname
+ *  Novel :name, year of release, author
+ *  authorage : age of author at year of release
+ *  isWrittenBy: author
+ *  copy(new year of release) : new instance of novel with new year of release i.e revised year of release
+ */
+ 
+class Writer(firstName :String, surname:String, val yearOfBirth:Int){
+  def fullName= firstName+" "+surname
+  
+}
+
+class Novel(name :String, yearOfRelease:Int, author:Writer){
+  //to use yearOfBirth in class novel as a field it has to be converted
+  //in writer class from params to field hence it is mentioned as val yearOfBirth
+  def authorAge = yearOfRelease - author.yearOfBirth 
+  def isWrittenBy(author:Writer) = author == this.author
+  
+}
+
+
+/*
+ * Create class counter 
+ * -receives 3 inputs
+ * -method current count
+ * -method to increment/decrement => new counter
+ * -overload inc dec to receive an amount*/
+ class Counter(val count:Int=0){
+   
+   def inc = new Counter(count+1)  
+    //Immutability, cannot modify the instance instead create a new instance whenever the content is modified
+   def dec = new Counter(count-1)
+   
+   def inc(n:Int):Counter={
+     if (n<=0) this
+     inc.inc(n+ 1)
+   }
+   
+   def dec(n:Int):Counter={
+     if(n<=0) this
+     dec.dec(n-1)
+   }
+   
+   def print=println(count)
+ 
+   
+ }
